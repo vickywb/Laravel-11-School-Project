d51c00e8f1340bdb5b1ea7ec67c08975310f59e6
vicky wibisono <wibivicky@gmail.com>
2024-05-04 12:29:33 +0700

    add create in frontend student register

--- app/Http/Controllers/Frontend/StudentRegisterController.php
+++ app/Http/Controllers/Frontend/StudentRegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StudentStoreRequest;
 use App\Models\Major;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentRegisterController extends Controller
@@ -16,13 +18,32 @@ class StudentRegisterController extends Controller
         ]);
     }
 
-    public function store(Request $request)
+    public function store(StudentStoreRequest $request, Student $student)
     {
-        //
-    }
+        $data = $request->only([
+            'name', 'nisn', 'gender', 'religion', 'place_of_birth', 'date_of_birth',
+            'address', 'phone_number', 'origin_of_school', 'address_of_school',
+            'father_name', 'mother_name', 'father_job', 'mother_job', 
+            'phone_number_parent'
+        ]);
 
-    public function show(string $id)
-    {
-        //
+        try {
+            DB::beginTransaction();
+
+            $student = new Student($data);
+            $student = $this->studentRepository->store($student);
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            return redirect()->back()->withErrors([
+                'errors', $th->getMessage()
+            ]);
+        }
+
+        return redirect()->route('form.pendaftaran')->with([
+            'success' => 'Selamat, Anda telah terdaftar sebagai murid disekolah ini.'
+        ]);
     }
 }
