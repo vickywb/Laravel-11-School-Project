6cf95c09cb7c945ca484fc751beb1067d370f680
vicky wibisono <wibivicky@gmail.com>
2024-05-11 10:19:13 +0700

    change image on hero page frontend

--- resources/views/frontend/hero.blade.php
+++ resources/views/frontend/hero.blade.php
@@ -4,22 +4,24 @@
       <div class="row d-flex justify-content-center">
           <div class="col-12">
               <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+
                   <div class="carousel-inner">
                     @foreach ($heroPages as $heroPage)
-                    <div class="carousel-item active">
-                        <img
-                            src="{{ $heroPage->file->showFile ?? asset('noimage/no-image.png') }}"
-                            class="d-block w-100"
-                            style="height: 564px"
-                            alt="client-image"
-                        />
-                        <div class="carousel-caption d-none d-md-block" style="padding-bottom: 0px; color: black">
-                            <h2 style="font-size: 36px; color: black;"><b>{{ $heroPage->title ?? 'Nama Sekolah' }}</b></h2>
-                            <p style="font-size: 18px"><b>{!! $heroPage->description ?? 'alamat sekolah atau yang lainya' !!}</b></p>
+                        <div class="carousel-item active">
+                            <img
+                                src="{{ $heroPage->file->showFile ?? asset('frontend/img/sekolah.jpg') }}"
+                                class="d-block w-100"
+                                style="height: 564px"
+                                alt="hero-page-image"
+                            />
+                            <div class="carousel-caption d-none d-md-block" style="padding-bottom: 0px; color: black">
+                                <h2 style="font-size: 36px; color: black;"><b>{{ $heroPage->title ?? 'Nama Sekolah' }}</b></h2>
+                                <p style="font-size: 18px"><b>{!! $heroPage->description ?? 'alamat sekolah atau yang lainya' !!}</b></p>
+                            </div>
                         </div>
-                    </div>
                     @endforeach
                   </div>
+                  
               </div>
           </div>
       </div>
